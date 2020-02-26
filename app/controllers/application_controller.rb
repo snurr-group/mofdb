392dd8a36583cd7ddf6f68d23cdc54ee8b99ecf8
n8ta <n8@u.northwestern.edu>
2020-02-26 14:23:54 -0600

    move to encrypted credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def verify_access
-    unless params[:passkey] == ENV['passkey']
+    unless params[:passkey] ==  Rails.application.credentials.api_passkey
       render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
     end
   end
