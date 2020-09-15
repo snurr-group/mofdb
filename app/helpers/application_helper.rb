ba6993dc9f89c4d57cdd8b9bcd4fc91c737f89fc
n8ta <n8@u.northwestern.edu>
2020-09-14 19:17:39 -0500

    gen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ require 'set'
 
 module ApplicationHelper
 
-  def fetch_or_gen(key, lambda, expiry=nil)
+  def fetch_or_gen(key, lambda, expiry = nil)
     val = Rails.cache.read(key)
     if val.nil?
       val = lambda.call
@@ -15,12 +15,12 @@ module ApplicationHelper
     if doi.nil?
       return db.name.gsub(/[^0-9a-z ]/i, ' ') + '.zip'
     end
-    return "#{db.name}-#{doi}-#{gases.nil? ? "all" : gases.to_a.map{|g|Gas.find(g).name}.join("-")}".gsub(/[^0-9a-z ]/i, ' ') + ".zip"
+    return "#{db.name}-#{doi}-#{gases.nil? ? "all" : gases.to_a.map { |g| g.name }.join("-")}".gsub(/[^0-9a-z ]/i, ' ') + ".zip"
   end
 
   def get_db_doi_gas_combos
     combinations = Rails.cache.read("combinations")
-    # combinations = nil
+    combinations = nil
     if (combinations.nil?)
       puts "CACHE MISS"
       combinations = {}
@@ -29,13 +29,21 @@ module ApplicationHelper
         puts db.name
         combinations[db] = {}
         dois = all_dois.select { |doi| Isotherm.find_by(doi: doi).mof.database == db }
+        puts "done finding dois"
         dois.each do |doi|
           gases = Set[]
-          count = 0
-          Isotherm.distinct.where(doi: doi).includes(:gases).find_each(batch_size: 1000) do |iso|
-            count += 1
-            puts count if count%1000 == 0
-            gases << iso.gases.pluck(:id).to_set
+          query = "SELECT DISTINCT JSON_OBJECTAGG(isodata.gas_id,'') from isotherms
+                   INNER JOIN isodata on isodata.isotherm_id = isotherms.id
+                   WHERE doi = (?)
+                   GROUP BY  isotherms.id;"
+          sanitized = ActiveRecord::Base.send(:sanitize_sql_array, [query, doi])
+          results = ActiveRecord::Base.connection.execute(sanitized)
+          results.each do |result|
+            new = JSON.parse(result[0]).keys.map { |v| Gas.find(v.to_i) }.to_set
+            puts "adding new"
+            puts new.map { |n| n.name }
+            puts "\n"
+            gases << new
           end
           combinations[db][doi] = gases
         end
