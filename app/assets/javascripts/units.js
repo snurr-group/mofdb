1d6c78d240c6a4d631409144c1dae0db586d8fc9
n8ta <n8@u.northwestern.edu>
2020-11-29 22:26:18 -0600

    auth token

--- app/assets/javascripts/units.js
+++ app/assets/javascripts/units.js
@@ -1,10 +1,19 @@
 $(document).on('DOMContentLoaded', function () {
     const unitsSelect = document.getElementById('supportedUnitsSelector')
+    let auth_token = document.querySelector("meta[name='csrf-token']").content;
     unitsSelect.addEventListener('change', (event) => {
         const units = event.target.value
-        $.post(`/setUnits?units=${units}`, (data) => {
+        $.ajax({url: `/setUnits?units=${units}`,
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': auth_token,
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+            success: (data) => {
                 window.location.reload()
-            }
+            }}
         );
     });
 
