832b24bbe70163f8b8bb6cb326540f39651d6ac1
n8ta <n8@u.northwestern.edu>
2021-03-25 15:34:43 -0500

    protect update route

--- app/controllers/forcefields_controller.rb
+++ app/controllers/forcefields_controller.rb
@@ -1,6 +1,6 @@
 class ForcefieldsController < ApplicationController
 
-  before_action :verify_access, only: [:create]
+  before_action :verify_access, only: [:create, :update]
   skip_forgery_protection only: [:create, :update]
   before_action :set_ff, only: [:update]
 
