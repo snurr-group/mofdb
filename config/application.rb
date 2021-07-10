5a94ac6c8d4053654a81323d99ac747b5375f766
n8ta <n8@u.northwestern.edu>
2021-07-10 15:52:36 -0500

    rm raven

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,5 @@ module Mofdb2
     config.hosts << "local.northwestern.edu"
     config.hosts << "mof.tech.northwestern.edu"
 
-    if Rails.env.production?
-      Raven.configure do |config|
-        config.dsn = 'https://25089bbc81df4bf1bb44fa71f8e29faa:4eaaa398ad8f4baa8145a7a50a68f76f@sentry.io/1828682'
-      end
-    end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
