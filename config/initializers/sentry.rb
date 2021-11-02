e03d9c02437e9c0807de7afd79f150b305bb350e
Nathaniel Tracy-Amoroso <nathanielt@microsoft.com>
2021-11-01 22:39:57 -0700

    reduce sample rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,9 +6,5 @@ unless Rails.env.development?
     # To activate performance monitoring, set one of these options.
     # We recommend adjusting the value in production:
     config.traces_sample_rate = 0.01
-    # or
-    config.traces_sampler = lambda do |context|
-      true
-    end
   end
 end
