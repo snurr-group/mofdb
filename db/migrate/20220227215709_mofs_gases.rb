ea9678422af58252c9c443c8815d33a520719fc7
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-02-27 14:27:04 -0800

    batches!

--- db/migrate/20220227215709_mofs_gases.rb
+++ db/migrate/20220227215709_mofs_gases.rb
@@ -1,10 +1,14 @@
 class MofsGases < ActiveRecord::Migration[6.0]
   def up
     create_join_table :mofs, :gases, unique: true
-    Mof.all.each do |mof|
-      mof.save
+    Mof.all.in_batches(of: 100).each do |batch|
+      puts "batch..."
+      batch.each do |mof|
+        mof.save
+      end
     end
   end
+
   def down
     drop_join_table :mofs, :gases
   end
