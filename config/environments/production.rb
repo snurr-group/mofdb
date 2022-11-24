ad1e69430a677649d1a2a5cd63cc8eb923abca49
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-11-24 12:04:17 -0800

    dont server static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.assets.compile = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
